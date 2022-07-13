bd74990c0ac328db16f01ec74ba916f9694230c9
Michael Platini <michael.platini@live.com>
2022-07-13 19:40:53 -0300

    aula6/

---
+++ aula6/Oper-aritméticos.js
@@ -0,0 +1,14 @@
+/* * Multiplicação
++ Adição/ Concatenação
+- Subitração
+/ Divisão 
+** Potenciação
+% Resto da divisão
+Incremento = ++
+Decremento = --
+Operadores de atribuição*/
+
+let contador = 2;
+
+contador **=10;
+console.log(contador);
